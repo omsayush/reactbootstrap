5092ec049f807e8d5557e7ea9e23892e9e086737
unknown <mail@ayushsrivastava.xyz>
2021-05-23 22:42:33 +0530

    my first commit reactbs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <React.Fragment>
+    <h1 className="text-capitalize text-center text-danger my-5">Welcome to React Bootstrap</h1>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm">
+        <div class="card">
+  <img src="https://picsum.photos/200/302" class="card-img-top" alt="..." height="300px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <a href="#" class="btn btn-primary">Click Here</a>
+  </div>
+</div>
+      </div>
+      <div className="col-sm">
+        <div class="card">
+  <img src="https://picsum.photos/200/301" class="card-img-top" alt="..." height="300px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <a href="#" class="btn btn-primary">Click Here</a>
+  </div>
+</div>
+      </div>
+      <div className="col-sm">
+        <div class="card">
+  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="300px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <a href="#" class="btn btn-primary">Click Here</a>
+  </div>
+</div>
+      </div>
     </div>
-  );
+  </div>
+    </React.Fragment>
+  )
 }
 
 export default App;
